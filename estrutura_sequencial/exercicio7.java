a19420d8ba7176afd7ba2d79e56a3e077e839596
Silas Rodrigues <silasnas2018@gmail.com>
2025-03-13 14:33:39 -0300

    exercicio7

---
+++ estrutura_sequencial/exercicio7.java
@@ -0,0 +1,24 @@
+package estrutura_sequencial;
+
+import java.util.*;
+
+public class exercicio7 {
+    public static void main(String[] args) {
+        int cod, saida;
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite o código do produto :");
+        cod = sc.nextInt();
+
+        if (cod == 1) {
+            System.out.printf("Código %d : Parafuso", cod);
+        }else if (cod == 2) {
+            System.out.printf("Código %d : Porca", cod);
+        }else if (cod == 3) {
+            System.out.printf("Código %d : Prego", cod);
+        }else{
+            System.out.printf("Código %d : Diversos",cod);
+        }
+    }    
+}
