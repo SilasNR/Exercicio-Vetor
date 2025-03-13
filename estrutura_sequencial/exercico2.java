e50bfc88d161ce98ea0980e570797059b1239156
Silas Rodrigues <silasnas2018@gmail.com>
2025-03-13 10:53:39 -0300

    exercico2

---
+++ estrutura_sequencial/exercico2.java
@@ -0,0 +1,25 @@
+package estrutura_sequencial;
+
+import java.util.Scanner;
+
+public class exercico2 {
+    public static void main(String[] args) {
+        float valorKg, quantKg, valorTotal;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o valor do quilo(kg) do produto : R$");
+        valorKg = sc.nextFloat();
+
+        System.out.print("Digite a quantidade de quilos(kg) consumidas : R$");
+        quantKg = sc.nextFloat();
+
+        valorTotal = valorKg * quantKg;
+
+        if (valorTotal < 0) {
+            System.out.println("Valores não aceitos !");
+        }else{
+            System.out.printf("Valor do quilo(kg) do produto : R$%.2f\nQuantidade consumida do mesmo : %.2fkg\nValor total a ser pago : R$%.2f", valorKg,quantKg,valorTotal);
+        }
+    }
+}
