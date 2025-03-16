3000dcd3fbe3244eb5b6b5005435a78be315334d
Silas Rodrigues <silasnas2018@gmail.com>
2025-03-16 12:31:07 -0300

    exercico26

---
+++ estrutura_sequencial/exercicio26.java
@@ -0,0 +1,26 @@
+package estrutura_sequencial;
+
+public class exercicio26 {
+    public static void main(String[] args) {
+        int a[] = { 32, 45, 89, 66, 12, 35, 10, 96, 38, 15, 13, 11, 65, 81, 35, 64, 16, 89, 54, 19 };
+
+        int n = a.length;
+
+        String b = "" ;
+        int m = 0;
+        int i;
+        String saida = "";
+        String indice = "indice: ";
+
+        for (i = 0; i < n; i++) {
+            if(a[i] < 50){
+                b = i +" "+ b ;
+            }
+        }
+
+
+        saida = "Resposta: " + b + /* "\n" + indice +*/"\nFim.";
+        System.out.println(saida);
+
+    }
+}
