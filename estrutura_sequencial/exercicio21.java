785a77577d0f199713d319302634ac168498d94c
Silas Rodrigues <silasnas2018@gmail.com>
2025-03-16 11:14:46 -0300

    exercico21

--- estrutura_sequencial/exercicio21.java
+++ estrutura_sequencial/exercicio21.java
@@ -1,26 +1,28 @@
 package estrutura_sequencial;
 
-public class exercico19 {
+public class exercicio21 {
     public static void main(String[] args) {
         int a[] = { 32, 45, 89, 66, 12, 35, 10, 96, 38, 15, 13, 11, 65, 81, 35, 64, 16, 89, 54, 19 };
 
         int n = a.length;
 
         int b[] = new int[n];
-        String m = "";
+        int m = 0;
         int i;
         String saida = "";
-        String indice = "";
+        String indice = "indice: ";
 
-        for (i = 0; i < n - 1; i++) {
-            if ((a[i]%2) == 0  ) {
+        for (i = 0; i < n; i++) {
+            
 
-                m = m + " " +a[i];
+                m = m + a[i];
                 indice = indice + " " + i;
-            }
+
         }
 
-        saida = "Resposta: " + m + "\nindice: " + indice +"\nFim.";
+        b[0] = m / n;
+
+        saida = "Resposta: " + b[0] + /* "\n" + indice +*/"\nFim.";
         System.out.println(saida);
 
     }
