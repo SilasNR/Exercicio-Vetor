8574f3faeb577759d689c0de1fe6331726cca779
Silas Rodrigues <silasnas2018@gmail.com>
2025-03-16 11:21:59 -0300

    exercicio22

---
+++ estrutura_sequencial/exercicio22.java
@@ -0,0 +1,29 @@
+package estrutura_sequencial;
+
+public class exercicio22 {
+    public static void main(String[] args) {
+        int a[] = { 32, 45, 89, 66, 12, 35, 10, 96, 38, 15, 13, 11, 65, 81, 35, 64, 16, 89, 54, 19 };
+
+        int n = a.length;
+
+        int b[] = new int[n];
+        int m = 0;
+        int i;
+        String saida = "";
+        String indice = "indice: ";
+
+        for (i = 0; i < n; i++) {
+            if((a[i]%2) == 1){
+                b[0] = b[0] + a[i];
+                indice = indice + " " + i;
+            }
+
+
+        }
+
+
+        saida = "Resposta: " + b[0] + /* "\n" + indice +*/"\nFim.";
+        System.out.println(saida);
+
+    }
+}
